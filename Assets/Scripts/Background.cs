837c096d17ad560b81aab09e8bbc0f2a52bd8780
Jason Stepp <info@jasonstepp.me>
2020-08-12 20:19:00 -0700

    Fixed collision detection, added scrolling backgrounds and starfield particle effects

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+
+    Material _material;
+    float currentscroll;
+    [SerializeField] float speed = 0.02f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _material = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentscroll += speed * Time.deltaTime;
+        _material.mainTextureOffset = new Vector2(0, currentscroll);
+    }
+}
